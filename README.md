06aec84b0af9f378c0b803995617e5e1e023393b
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2022-06-05 17:49:05 +0300

    Code complete :+1:

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# smooth-servo-movement
- Servo motors can be jerky sometimes.
+
+# Smooth Servo Movement
+
+Servo motors can be jerky sometimes.
+Moving from one position to the other too fast, making these mostly ignorable jerks.
+
+We can smoothen the servo movements by controlling our acceleration and deceleration.
+
+This code only works with minimal to no delays for the smoothing operation. For more complex
+projects, check out **multitasking on the arduino (Ditch that Delay).**
+
+## Acknowledgements
+
+The source code used in this project is based on the following video by James Bruton.
+
+- [How To Make Robots Move Smoothly | Arduino Tutorial](https://youtu.be/jsXolwJskKM?list=TLPQMDQwNjIwMjISkcZBB2FYFg)
+
+## License
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://choosealicense.com/licenses/mit/)
--- smooth-servo-movement/src/main.cpp
+++ smooth-servo-movement/src/main.cpp
@@ -1,9 +1,52 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// Servo connected to pin 8 on the MEGA 2560 [PWM]
+int signalPin = 8;
+
+// Potentiometer to control the Servo
+int potentiometer = A0;
+
+// Create a Servo instance
+Servo myServo;
+
+int value;
+int potValue;
+float valueSmoothed;
+float valuePrevious;
+
+void setup()
+{
+  // set up Servo instance
+  myServo.attach(signalPin);
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
+  // Reading ranges from 0 to 1023
+  potValue = analogRead(potentiometer);
+
+  // Map the reading to the Servo's range
+  value = map(potValue, 0, 1023, 0, 180);
+
+  //* SMOOTHING
+
+  valueSmoothed = (value * 0.05) + (valuePrevious * 0.95);
+  valuePrevious = valueSmoothed;
+
+  //* END OF SMOOTHING
+
+  // you can use Arduino's Serial Plotter to view the data.
+  Serial.print(potValue);
+  Serial.print(" --- ");
+  Serial.print(value);
+  Serial.print(" --- ");
+  Serial.println(valueSmoothed);
+
+  // Write the smoothed value to the Servo instance.
+  myServo.write(valueSmoothed);
+
+  delay(10);
 }
